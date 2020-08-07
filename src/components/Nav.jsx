aec2cfb278455f7fa7ceff80958f9db3bd32f619
wardou2 <ward92@gmail.com>
2020-08-07 18:28:56 -0400

    fix styling for nav hamburger. added text Navigation on mobile

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,8 +8,13 @@ const Nav = (props) => {
     return (
         <Grid>
             {props.isMobile ? (
-                <Grid.Row>
-                    <Grid.Column width={2} floated="right">
+                <Grid.Row >
+                    <Grid.Column width={8} />
+                    <Grid.Column textAlign="right" width={8}>
+                        <span className="item font-heading">
+                            Navigation{"   "}
+                        </span>
+                        <Icon name="angle double right" color="black" size="small"/>
                         <Link className="item" onClick={props.toggleSidebar} to="">
                             <Icon color="black" name="bars" size="large" />
                         </Link>
